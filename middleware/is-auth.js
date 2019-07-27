028f172a9022a1b29f17b2ac6411d58d7196cbca
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-07-27 14:48:37 +1200

    Add status 201 to post requests that create resources

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,7 @@
 // Prevents uauthorized users from accessing restricted routes
 module.exports = (req, res, next) => {
   if (!req.session.authenticated) {
-    return res.redirect('/login');
+    return res.status(403).redirect('/login');
   }
   next();
 }
